44fc307aeeb67e712ca814c5c6880664fe50bea2
Mihir Kapadia <mihir@duneolas.com>
2025-07-25 16:21:57 +0530

    test

---
+++ dags/dbt_dag.py
@@ -0,0 +1,9 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from pendulum import datetime
+
+with DAG("check_dbt_installed", start_date=datetime(2024, 1, 1), schedule=None, catchup=False):
+    check_dbt = BashOperator(
+        task_id="check_dbt",
+        bash_command="which dbt || echo 'dbt not installed'"
+    )

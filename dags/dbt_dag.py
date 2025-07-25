af1eef2d0ff13b714e5fa82d793d2c99facf57fd
Mihir Kapadia <mihir@duneolas.com>
2025-07-25 16:30:24 +0530

    test

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -5,5 +5,5 @@ from pendulum import datetime
 with DAG("check_dbt_installed", start_date=datetime(2024, 1, 1), schedule=None, catchup=False):
     check_dbt = BashOperator(
         task_id="check_dbt",
-        bash_command="which dbt || echo 'dbt not installed'"
+        bash_command="which dbt && dbt --version"
     )

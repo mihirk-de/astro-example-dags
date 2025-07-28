ec88e69766c184042dc28049ebdff64bc1fe38f1
Mihir Kapadia <mihir@duneolas.com>
2025-07-28 18:28:39 +0530

    added system s test models

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -17,8 +17,20 @@ with DAG("run_system_s_dbt_features", start_date=datetime(2024, 1, 1), schedule=
 # dbt run --profiles-dir /usr/local/airflow/dbt/profiles
 # """
 #     )
-    tasks = []
-    for feature in features:
+    # tasks = []
+    # for feature in features:
+    #     task = BashOperator(
+    #         task_id=f"run_{feature.replace('.', '_')}",
+    #         bash_command=f"""
+    #         echo "$SNOWFLAKE_PRIVATE_KEY_BASE64" | base64 -d > /tmp/rsa_key.p8 && 
+    #         export SNOWFLAKE_PRIVATE_KEY_PATH=/tmp/rsa_key.p8 && 
+    #         cd /usr/local/airflow/dbt/my_dbt_proj && 
+    #         dbt build --select {feature} --profiles-dir /usr/local/airflow/dbt/profiles
+    #         """
+    #     )
+    #     tasks.append(task)
+
+    for i, feature in enumerate(features):
         task = BashOperator(
             task_id=f"run_{feature.replace('.', '_')}",
             bash_command=f"""
@@ -28,4 +40,7 @@ with DAG("run_system_s_dbt_features", start_date=datetime(2024, 1, 1), schedule=
             dbt build --select {feature} --profiles-dir /usr/local/airflow/dbt/profiles
             """
         )
+        if i > 0:
+            tasks[-1] >> task  # serial chaining
         tasks.append(task)
+
